181fb9794e3bd24fda1b588f234230996c871a72
Faza01 <91936066+Faza01@users.noreply.github.com>
2022-01-27 09:52:01 +0700

    modul 3

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
     public function index()
     {
         return view('contacts', [
-            "title" => "Contsacts"
+            "title" => "Contacts Us"
         ]);
     }
 
