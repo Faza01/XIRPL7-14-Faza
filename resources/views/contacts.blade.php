acd6bf704701ebb4603e4c3ad52d7073a0f0f2aa
Faza01 <91936066+Faza01@users.noreply.github.com>
2022-02-10 09:31:29 +0700

    model 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,17 +4,18 @@
 <div class="title-text mb-5">
     <h2 class="fw-bold">Contacts Us</h2>
 </div>
-<form>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
   <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="inputname" placeholder="nama">
+    <input type="text" class="form-control" id="inputname" placeholder="nama" name="nama">
     <label for="inputname">Nama</label>
   </div>
   <div class="form-floating mb-3">
-    <input type="email" class="form-control" id="inputemail" placeholder="email">
+    <input type="email" class="form-control" id="inputemail" placeholder="email" name="email">
     <label for="inputemail">Email</label>
   </div>
   <div class="form-floating mb-3">
-    <textarea class="form-control" placeholder="Tinggalkan pesan disini" id="floatingTextarea2" style="height : 100px"></textarea>
+    <textarea class="form-control" placeholder="Tinggalkan pesan disini" id="floatingTextarea2" style="height : 100px" name="pesan" ></textarea>
     <label for="floatingTextarea2">Pesan</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
